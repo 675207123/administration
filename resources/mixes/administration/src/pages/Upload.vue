93d8bbafa91363a39b3f37e50983e698625cdde7
twilroad <269044570@qq.com>
2017-04-13 16:14:24 +0800

    add title with translation for upload

--- resources/mixes/administration/src/pages/Upload.vue
+++ resources/mixes/administration/src/pages/Upload.vue
@@ -1,5 +1,5 @@
 <script>
-    import injection from '../helpers/injection';
+    import injection, { trans } from '../helpers/injection';
 
     export default {
         beforeRouteEnter(to, from, next) {
@@ -134,6 +134,9 @@
                     }
                 });
             },
+        },
+        mounted() {
+            this.$store.commit('title', trans('administration.title.upload'));
         },
     };
 </script>
