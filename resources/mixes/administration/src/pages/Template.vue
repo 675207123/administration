19edc7b16fb1c769c353539cabeb6038b77c1716
twilroad <269044570@qq.com>
2017-04-13 16:49:12 +0800

    add title with translation for template

--- resources/mixes/administration/src/pages/Template.vue
+++ resources/mixes/administration/src/pages/Template.vue
@@ -1,11 +1,14 @@
 <script>
-    import injection from '../helpers/injection';
+    import injection, { trans } from '../helpers/injection';
 
     export default {
         beforeRouteEnter(to, from, next) {
             next(() => {
                 injection.sidebar.active('setting');
             });
+        },
+        mounted() {
+            this.$store.commit('title', trans('administration.title.template'));
         },
     };
 </script>
