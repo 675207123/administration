a94fa50dc01f21bb1d261fb610dd757b25afcd13
twilroad <269044570@qq.com>
2017-06-14 12:14:43 +0800

    add entity for administration flow

---
+++ src/Entities/Administration.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-06-14 12:04
+ */
+namespace Notadd\Administration\Entities;
+
+use Notadd\Foundation\Flow\Abstracts\Entity;
+use Symfony\Component\Workflow\Event\GuardEvent;
+
+/**
+ * Class Administration.
+ */
+class Administration extends Entity
+{
+    /**
+     * Definition of name for flow.
+     *
+     * @return string
+     */
+    public function name()
+    {
+        // TODO: Implement name() method.
+    }
+
+    /**
+     * Definition of places for flow.
+     *
+     * @return array
+     */
+    public function places()
+    {
+        // TODO: Implement places() method.
+    }
+
+    /**
+     * Definition of transitions for flow.
+     *
+     * @return array
+     */
+    public function transitions()
+    {
+        // TODO: Implement transitions() method.
+    }
+
+    /**
+     * Announce a transition.
+     */
+    public function announce()
+    {
+        // TODO: Implement announce() method.
+    }
+
+    /**
+     * Enter a place.
+     */
+    public function enter()
+    {
+        // TODO: Implement enter() method.
+    }
+
+    /**
+     * Entered a place.
+     */
+    public function entered()
+    {
+        // TODO: Implement entered() method.
+    }
+
+    /**
+     * Guard a transition.
+     *
+     * @param \Symfony\Component\Workflow\Event\GuardEvent $event
+     */
+    public function guard(GuardEvent $event)
+    {
+        // TODO: Implement guard() method.
+    }
+
+    /**
+     * Leave a place.
+     */
+    public function leave()
+    {
+        // TODO: Implement leave() method.
+    }
+
+    /**
+     * Into a transition.
+     */
+    public function transition()
+    {
+        // TODO: Implement transition() method.
+    }
+}
