213405f323f18bca05ac91f913679ed6390e350b
twilroad <269044570@qq.com>
2017-08-15 13:49:27 +0800

    return domain info while getting module info

--- src/Handlers/ModuleHandler.php
+++ src/Handlers/ModuleHandler.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Collection;
 use Notadd\Foundation\Module\Module;
 use Notadd\Foundation\Module\ModuleManager;
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Foundation\Setting\Contracts\SettingsRepository;
 
 /**
  * Class ModuleHandler.
@@ -25,15 +26,22 @@ class ModuleHandler extends Handler
     protected $manager;
 
     /**
+     * @var \Notadd\Foundation\Setting\Contracts\SettingsRepository
+     */
+    private $setting;
+
+    /**
      * ModuleHandler constructor.
      *
-     * @param \Illuminate\Container\Container         $container
-     * @param \Notadd\Foundation\Module\ModuleManager $manager
+     * @param \Illuminate\Container\Container                         $container
+     * @param \Notadd\Foundation\Module\ModuleManager                 $manager
+     * @param \Notadd\Foundation\Setting\Contracts\SettingsRepository $setting
      */
-    public function __construct(Container $container, ModuleManager $manager)
+    public function __construct(Container $container, ModuleManager $manager, SettingsRepository $setting)
     {
         parent::__construct($container);
         $this->manager = $manager;
+        $this->setting = $setting;
     }
 
     /**
@@ -43,16 +51,37 @@ class ModuleHandler extends Handler
      */
     protected function execute()
     {
-        $all = $this->manager->getModules();
+        $modules = $this->manager->getModules();
         $enabled = $this->manager->getEnabledModules();
         $installed = $this->manager->getInstalledModules();
         $notInstalled = $this->manager->getNotInstalledModules();
-        $all->offsetUnset('notadd/administration');
+        $modules->offsetUnset('notadd/administration');
+        $domains = $modules->map(function (Module $module) {
+            $data = [];
+            $alias = 'module.' . $module->identification() . '.domain.alias';
+            $enabled = 'module.' . $module->identification() . '.domain.enabled';
+            $host = 'module.' . $module->identification() . '.domain.host';
+            $data['identification'] = $module->identification();
+            $data['name'] = $module->name();
+            $data['alias'] = $this->setting->get($alias, '');
+            $data['enabled'] = boolval($this->setting->get($enabled, 0));
+            $data['host'] = $this->setting->get($host, '');
+
+            return $data;
+        });
+        $domains->prepend([
+            'alias' => '/',
+            'enabled' => boolval($this->setting->get('module.notadd/notadd.domain.enabled', 0)),
+            'host' => $this->setting->get('module.notadd/notadd.domain.host', ''),
+            'identification' => 'notadd/notadd',
+            'name' => 'Notadd',
+        ], 'notadd/notadd');
         $enabled->offsetUnset('notadd/administration');
         $installed->offsetUnset('notadd/administration');
         $notInstalled->offsetUnset('notadd/administration');
         $this->withCode(200)->withData([
-            'all'        => $this->info($all),
+            'all'        => $this->info($modules),
+            'domains'    => $domains->toArray(),
             'enabled'    => $this->info($enabled),
             'installed'  => $this->info($installed),
             'notInstall' => $this->info($notInstalled),
@@ -72,7 +101,7 @@ class ModuleHandler extends Handler
         $list->each(function (Module $module) use ($data) {
             $data->put($module->identification(), [
                 'author'         => collect($module->author())->implode(','),
-                'enabled'        => $module->enabled(),
+                'enabled'        => boolval($module->enabled()),
                 'description'    => $module->description(),
                 'identification' => $module->identification(),
                 'name'           => $module->name(),
