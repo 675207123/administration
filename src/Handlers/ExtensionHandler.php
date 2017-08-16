2e88714642406589b3f76bf901ed991eb0438b66
twilroad <269044570@qq.com>
2017-08-16 17:48:25 +0800

    fix getters for extension informations

--- src/Handlers/ExtensionHandler.php
+++ src/Handlers/ExtensionHandler.php
@@ -14,6 +14,9 @@ use Notadd\Foundation\Extension\Extension;
 use Notadd\Foundation\Extension\ExtensionManager;
 use Notadd\Foundation\Routing\Abstracts\Handler;
 
+/**
+ * Class ExtensionHandler.
+ */
 class ExtensionHandler extends Handler
 {
     /**
@@ -45,9 +48,9 @@ class ExtensionHandler extends Handler
         $installed = $this->manager->getInstalledExtensions();
         $notInstalled = $this->manager->getNotInstalledExtensions();
         $this->withCode(200)->withData([
-            'all' => $this->info($all),
-            'enabled' => $this->info($enabled),
-            'installed' => $this->info($installed),
+            'all'        => $this->info($all),
+            'enabled'    => $this->info($enabled),
+            'installed'  => $this->info($installed),
             'notInstall' => $this->info($notInstalled),
         ])->withMessage('获取插件列表成功！');
     }
@@ -59,15 +62,16 @@ class ExtensionHandler extends Handler
      *
      * @return array
      */
-    protected function info(Collection $list) {
-        $data = new Collection();
-        $list->each(function (Extension $extension) use($data) {
-            $data->put($extension->getIdentification(), [
-                'author' => collect($extension->getAuthor())->implode(','),
-                'enabled' => $extension->isEnabled(),
-                'description' => $extension->getDescription(),
-                'identification' => $extension->getIdentification(),
-                'name' => $extension->getName(),
+    protected function info(Collection $list)
+    {
+        $data = collect();
+        $list->each(function (Extension $extension) use ($data) {
+            $data->put($extension->identification(), [
+                'author'         => collect($extension->offsetGet('author'))->implode(','),
+                'enabled'        => $extension->isEnabled(),
+                'description'    => $extension->offsetGet('description'),
+                'identification' => $extension->identification(),
+                'name'           => $extension->offsetGet('name'),
             ]);
         });
 
