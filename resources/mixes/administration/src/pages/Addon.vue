8318fc39c3b0c098e494566077f763e02aa92bf1
twilroad <269044570@qq.com>
2017-09-24 18:32:33 +0800

    add something

--- resources/mixes/administration/src/pages/Addon.vue
+++ resources/mixes/administration/src/pages/Addon.vue
@@ -5,7 +5,7 @@
     export default {
         beforeRouteEnter(to, from, next) {
             injection.loading.start();
-            injection.http.post(`${window.api}/administration/extension`).then(response => {
+            injection.http.post(`${window.api}/administration/addon`).then(response => {
                 next(vm => {
                     injection.loading.finish();
                     injection.sidebar.active('setting');
