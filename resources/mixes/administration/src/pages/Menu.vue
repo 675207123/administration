60a434ce4f0dec68f4723021ae22c6b665875b27
twilroad <269044570@qq.com>
2017-09-25 22:09:14 +0800

    add data binding for menu page

--- resources/mixes/administration/src/pages/Menu.vue
+++ resources/mixes/administration/src/pages/Menu.vue
@@ -1,5 +1,6 @@
 <script>
     import injection from '../helpers/injection';
+    import MenuChildren from './MenuChildren.vue';
 
     export default {
         beforeRouteEnter(to, from, next) {
@@ -7,104 +8,47 @@
             injection.http.get(`${window.api}/administration/menus`).then(response => {
                 next(vm => {
                     vm.items = Object.keys(response.data.data).map(index => response.data.data[index]);
-                    vm.originals = Object.keys(response.data.originals).map(index => response.data.originals[index]);
+                    Object.keys(response.data.originals).forEach(index => {
+                        response.data.originals[index].expand = false;
+                    });
+                    window.console.log(response.data.originals);
+                    vm.originals = response.data.originals;
                     injection.loading.finish();
                     injection.sidebar.active('setting');
                 });
             });
         },
+        components: {
+            MenuChildren,
+        },
         data() {
-            const self = this;
-
             return {
-                columns: [
-                    {
-                        render(h) {
-                            return h('p', {
-                                style: {
-                                    textAlign: 'center',
-                                },
-                            }, '子级菜单，尽请期待！');
-                        },
-                        type: 'expand',
-                        width: 50,
-                    },
-                    {
-                        key: 'order',
-                        render(h, data) {
-                            const store = data.row;
-                            if (store.identification === 'global') {
-                                return '0';
-                            }
-
-                            return h('tooltip', {
-                                props: {
-                                    placement: 'right-end',
-                                },
-                                scopedSlots: {
-                                    content() {
-                                        return '回车以修改数据';
-                                    },
-                                    default() {
-                                        return h('i-input', {
-                                            on: {
-                                                'on-change': event => {
-                                                    store.order = event.target.value;
-                                                },
-                                                'on-enter': () => {
-                                                    self.list[data.index].order = store.order;
-                                                    self.update();
-                                                },
-                                            },
-                                            props: {
-                                                number: true,
-                                                value: self.list[data.index].order,
-                                            },
-                                        });
-                                    },
-                                },
-                            });
-                        },
-                        title: '排序',
-                        width: 200,
-                    },
-                    {
-                        key: 'text',
-                        title: '菜单名称',
-                    },
-                    {
-                        key: 'show',
-                        render(h, data) {
-                            return h('i-switch', {
-                                on: {
-                                    'on-change': value => {
-                                        self.list[data.index].show = value;
-                                        self.update();
-                                    },
-                                },
-                                props: {
-                                    size: 'large',
-                                    value: data.row.show,
-                                },
-                                scopedSlots: {
-                                    close() {
-                                        return h('span', '关闭');
-                                    },
-                                    open() {
-                                        return h('span', '开启');
-                                    },
-                                },
-                            });
-                        },
-                        title: '是否显示',
-                        width: 200,
-                    },
-                ],
                 items: [],
-                originals: [],
+                originals: {},
+                rules: {
+                    order: [
+                        {
+                            message: '排序不能为空',
+                            required: true,
+                            trigger: 'change',
+                            type: 'number',
+                        },
+                    ],
+                    text: [
+                        {
+                            required: true,
+                            message: '菜单名称不能为空',
+                            trigger: 'change',
+                            type: 'string',
+                        },
+                    ],
+                },
             };
         },
         methods: {
+            expand(index) {
+                this.originals[index].expand = !this.originals[index].expand;
+            },
             update() {
                 const self = this;
                 self.$loading.start();
@@ -123,6 +67,14 @@
                 });
             },
         },
+        watch: {
+            originals: {
+                deep: true,
+                handler(val) {
+                    window.console.log(val);
+                },
+            },
+        },
     };
 </script>
 <template>
@@ -130,7 +82,115 @@
         <tabs>
             <tab-pane label="菜单管理">
                 <card :bordered="false">
-                    <i-table :columns="columns" :data="items"></i-table>
+                    <i-form :model="originals" ref="form">
+                        <div class="ivu-table-wrapper">
+                            <div class="ivu-table">
+                                <div class="ivu-table-header">
+                                    <table cellspacing="0" cellpadding="0" border="0" width="100%">
+                                        <colgroup>
+                                            <col width="60">
+                                            <col width="200">
+                                            <col>
+                                            <col width="200">
+                                        </colgroup>
+                                        <thead>
+                                        <tr>
+                                            <th></th>
+                                            <th>
+                                                <div class="ivu-table-cell"><span>排序</span></div>
+                                            </th>
+                                            <th>
+                                                <div class="ivu-table-cell"><span>菜单名称</span></div>
+                                            </th>
+                                            <th>
+                                                <div class="ivu-table-cell"><span>是否开启</span></div>
+                                            </th>
+                                        </tr>
+                                        </thead>
+                                    </table>
+                                </div>
+                                <div class="ivu-table-body" v-if="items.length">
+                                    <table cellspacing="0" cellpadding="0" border="0" width="100%">
+                                        <colgroup>
+                                            <col width="60">
+                                            <col width="200">
+                                            <col>
+                                            <col width="300">
+                                        </colgroup>
+                                        <tbody class="ivu-table-tbody">
+                                        <template v-for="(item, index) in items">
+                                            <tr class="ivu-table-row">
+                                                <td>
+                                                    <div class="ivu-table-cell ivu-table-cell-with-expand"
+                                                         v-if="Object.keys(item.children).length > 0"
+                                                         @click="expand(item.index)">
+                                                        <template v-if="originals[item.index].expand">
+                                                            <div class="ivu-table-cell-expand ivu-table-cell-expand-expanded">
+                                                                <div class="ivu-table-cell-expand">
+                                                                    <i class="ivu-icon ivu-icon-ios-arrow-right"></i>
+                                                                </div>
+                                                            </div>
+                                                        </template>
+                                                        <template v-else>
+                                                            <div class="ivu-table-cell-expand">
+                                                                <div class="ivu-table-cell-expand">
+                                                                    <i class="ivu-icon ivu-icon-ios-arrow-right"></i>
+                                                                </div>
+                                                            </div>
+                                                        </template>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="ivu-table-cell">
+                                                        <form-item label=""
+                                                                   :prop="item.index + '.order'"
+                                                                   :rules="rules.order"
+                                                                   style="margin-bottom: 0;">
+                                                            <i-input :number="true" placeholder="请输入规则排序" v-model="originals[item.index].order"></i-input>
+                                                        </form-item>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="ivu-table-cell">
+                                                        <form-item label=""
+                                                                   :prop="item.index + '.text'"
+                                                                   :rules="rules.text"
+                                                                   style="margin-bottom: 0;">
+                                                            <i-input placeholder="请输入菜单名称" v-model="originals[item.index].text"></i-input>
+                                                        </form-item>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="ivu-table-cell">
+                                                        <form-item label=""
+                                                                   :prop="item.index + '.text'"
+                                                                   :rules="rules.text"
+                                                                   style="margin-bottom: 0;">
+                                                            <i-switch v-model="originals[item.index].enabled"></i-switch>
+                                                        </form-item>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <menu-children :items="item.children"
+                                                           :originals="originals"
+                                                           :rules="rules"
+                                                           v-if="Object.keys(item.children).length > 0"></menu-children>
+                                        </template>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="ivu-table-tip" v-if="items.length === 0">
+                                    <table cellspacing="0" cellpadding="0" border="0">
+                                        <tbody>
+                                        <tr>
+                                            <td><span>暂无筛选结果</span></td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </i-form>
                 </card>
             </tab-pane>
         </tabs>
