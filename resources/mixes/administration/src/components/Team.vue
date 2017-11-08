6db9b4399ab3dc9dfcaf4f6f4f0777ed4d70b611
zhanghuan123 <zhhu_123@163.com>
2017-11-08 16:31:56 +0800

    Add swiper

---
+++ resources/mixes/administration/src/components/Team.vue
@@ -0,0 +1,36 @@
+<script>
+    import { swiper, swiperSlide } from 'vue-awesome-swiper';
+
+    export default {
+        components: {
+            swiper,
+            swiperSlide,
+        },
+        props: {
+            list: {
+                type: Array,
+                required: requireed,
+            },
+        },
+    };
+</script>
+<style lang="less">
+
+</style>
+<template>
+    <div class="team-wraper">
+        <div class="swiper-box">
+            <swiper :options="swiperOption">
+                <swiper-slide v-for="(slide, index) in list"
+                              :key="index"
+                              :class="{'active-slide': activeIndex === index}"
+                              @click.native="switchActive(index)">
+                    <img :src="slide.image" alt="">
+                    <p>{{ slide.name }}</p>
+                </swiper-slide>
+                <div class="swiper-button-prev hidden-xs" slot="button-prev"></div>
+                <div class="swiper-button-next hidden-xs" slot="button-next"></div>
+            </swiper>
+        </div>
+    </div>
+</template>
